49138655f61b9746bcae05a2dd773537be600203
Patrick Seiter <patrick.seiter@gmail.com>
2013-05-24 00:24:15 -0500

    Version from 2013/04/26 at 10:40 AM that removes fluxuations from the report file.

--- netcheck.sh
+++ netcheck.sh
@@ -11,16 +11,15 @@ ROOT=~/netcheck/
 LOG=netcheck.log
 LOCK=netcheck.lock
 DATE="+%Y-%m-%d %R:%S"
+FLUX=30
 #TIME="date +%R:%S"
 
 if [ -f "$ROOT$LOCK" ]; then
-	#echo "`date "$DATE"` Not running locked netcheck." | tee -a "$ROOT$LOG"
 	#echo "`date "$DATE"` Not running locked netcheck." >> "$ROOT$LOG"
 	exit
 else
 	touch $ROOT$LOCK
 	#echo "`date "$DATE"` Locking netcheck." | tee -a "$ROOT$LOG"
-	echo "`date "$DATE"` Locking netcheck." >> "$ROOT$LOG"
 fi
 
 WEBFILE=${ROOT}website.txt
@@ -34,38 +33,39 @@ FILE=${ROOT}netcheck.txt
 
 LOOP=true
 NODATE=true
-DOWNTIME=""
-UPTIME=""
+export DOWNTIME="0"
+export UPTIME="0"
 while $LOOP; do
-	#echo "LOOP: $LOOP"
-	#echo "NODATE: $NODATE"
 	if [ -n `cat $WEBFILE` ]; then
 		WEBSITE=`cat $WEBFILE`
 	fi
 	ping -c1 $WEBSITE > /dev/null 2>&1
 	RESULT=$?
-	#echo "RESULT: $RESULT"
 	if [ "$RESULT" -ne 0 ] && $NODATE; then
-		export DOWNTIME=`date "$DATE"`
-		#echo "$DOWNTIME Network went down." | tee -a "$ROOT$LOG"
+		DOWNTIME=`date "$DATE"`
 		echo "$DOWNTIME Network went down." >> "$ROOT$LOG"
+
 		NODATE=false
 	elif [ "$RESULT" -eq 0 ] && ! $NODATE; then
-		export UPTIME=`date "$DATE"`
-		#echo "$UPTIME Network came up." | tee -a "$ROOT$LOG"
+		UPTIME=`date "$DATE"`		
 		echo "$UPTIME Network came up." >> "$ROOT$LOG"
+
 		if [ ! -f "$FILE" ]; then
 			touch $FILE
-			#echo "`date "$DATE"` Created $FILE." | tee -a "$ROOT$LOG"
-			echo "`date "$DATE"` Created $FILE." >> "$ROOT$LOG"
+			echo "$UPTIME Created $FILE." >> "$ROOT$LOG"
 		fi
-		#echo "$DOWNTIME--$UPTIME" | tee -a $FILE
-		echo "$DOWNTIME--$UPTIME" >> $FILE
-		#echo "Added downtime $DOWNTIME--$UPTIME to $FILE." | tee -a "$ROOT$LOG"
+		
+		D=`date -d "$DOWNTIME" "+%s"`
+		U=`date -d "$UPTIME" "+%s"`
+		DIFF=$((U - D))
+		if [ $DIFF -gt $FLUX ]; then
+			echo "$DOWNTIME--$UPTIME" >> $FILE
+		else
+			echo "$DOWNTIME Downtime of $DIFF seconds skipped for being <= $FLUX." >> $ROOT$LOG
+		fi
 		#echo "Added downtime $DOWNTIME--$UPTIME to $FILE." >> "$ROOT/$LOG"
 		LOOP=false
 	elif [ "$RESULT" -eq 0 ]; then
-		#echo "`date "$DATE"` Network up." | tee -a "$ROOT/$LOG"
 		#echo "`date "$DATE"` Network up." >> "$ROOT/$LOG"
 		LOOP=false
 	fi
